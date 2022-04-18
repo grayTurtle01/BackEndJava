8d733217ebfa498712442f3d85afee2d1effbe12
grayTurtle01 <the.correo@protonmail.com>
2022-04-18 08:18:35 -0500

    harry potter object

--- 1_Modelado/Modelado.java
+++ 1_Modelado/Modelado.java
@@ -1,5 +1,45 @@
+class Personaje{
+    String name;
+    String house;
+    String gender;
+    String patronus;
+    String boggart;
+
+    public Personaje(String name, String house, String gender, String patronus, String boggart){
+        this.name = name;
+        this.house = house;
+        this.gender = gender;
+        this.patronus = patronus;
+        this.boggart = boggart;
+    }
+
+    public String toString(){
+        return String.format("%s | %s | %s | %s | %s",
+                             name, house, gender, patronus, boggart);
+    }
+
+    public void setPatronus(String patronus){
+        this.patronus = patronus;
+    }
+
+    public String getPatronus(String patronus){
+        return patronus;
+    }
+
+    public void setBoggart(String boggart){
+        this.boggart = boggart;
+    }
+
+    public String getBoggart(String boggart){
+        return boggart;
+    }
+}
+
 public class Modelado{
     public static void main(String[] args){
-        System.out.println("Hello Modelado");
+        Personaje harry = new Personaje("Harry", "Gryffindor", "Male", "Ciervo", "Dementor");
+       
+        System.out.println(harry);
+        
     }
 }
