c3f77c7fdf1c8b305d5dd7f7d1b08d6cc41efa42
grayTurtle01 <the.correo@protonmail.com>
2022-04-19 13:36:44 -0500

    add: Akali Class

---
+++ 3_Interfaces/Akali.java
@@ -0,0 +1,48 @@
+
+public class Akali extends Champion implements Moves{
+    
+    public Akali(){
+        super( "Akali",  "Asesino",  "Asesina Furtiva",  "Moderate");
+
+        pasive = "Marca de la Asesina";
+        firstMove = "Ráfaga de los 5 filos";
+        secondMove = "Manto Crepuscular";
+        thirdMove = "Maniobra de Shuriken";
+        ultimate = "Ejecución Perfecta";
+    }
+
+    public void pasive(){
+        System.out.println(pasive + ": Aumenta el daño en el próximo ataque");
+    }
+
+    public void firstMove_Q(){
+        System.out.println(firstMove + ": Lanza 5 kunais que relantizan");
+    }
+
+    public void secondMove_W(){
+        System.out.println(secondMove + ": Crea un anillo de humo donde gana invisibilidad");
+    }
+
+    public void thirdMove_E(){
+        System.out.println(thirdMove + ": Arroja un shoriken con el que puede marcar al enemigo");
+    }
+
+    public void ultimate_R(){
+        System.out.println(ultimate + ": Enviste a un enemigo, se lanza otra vez");
+    }
+
+    public String toString(){
+        return name + " : " + description;
+    }
+
+    public void showMoves(){
+        System.out.println("Moves: ");
+        pasive();
+        firstMove_Q();
+        secondMove_W();
+        thirdMove_E();
+        ultimate_R();
+    }
+
+}
+
--- 3_Interfaces/Interfaces.java
+++ 3_Interfaces/Interfaces.java
@@ -3,12 +3,13 @@ public class Interfaces{
 
     public static void main(String[] args){
 
-        //~ Ashe ashe = new Ashe();
-        //~ ashe.pasive();
-        //~ ashe.firstMove_Q();
-        //~ ashe.secondMove_W();
-        //~ ashe.thirdMove_E();
-        //~ ashe.ultimate_R();
+        Ashe ashe = new Ashe();
+        ashe.pasive();
+        ashe.firstMove_Q();
+        ashe.secondMove_W();
+        ashe.thirdMove_E();
+        ashe.ultimate_R();
+        System.out.println();
 
         LeeSin lee = new LeeSin();
         lee.pasive();
@@ -16,6 +17,11 @@ public class Interfaces{
         lee.secondMove_W();
         lee.thirdMove_E();
         lee.ultimate_R();
+        System.out.println();
+
+        Akali akali = new Akali();
+        System.out.println(akali);
+        akali.showMoves();
 
         
     }
