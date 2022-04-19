a1788635da5e6993fbb88e0bc53e38115d04b657
grayTurtle01 <the.correo@protonmail.com>
2022-04-19 08:52:04 -0500

    add: UmbrellaCat class

--- 2_practicaMona/Polimorfismo.java
+++ 2_practicaMona/Polimorfismo.java
@@ -36,18 +36,41 @@ class DinoCat extends Octocat{
     }
 }
 
+
+class UmbrellaCat extends Octocat{
+
+    public UmbrellaCat(){
+        super();
+        name = "UmbrellaCat";
+        description = "An octocat with an umbrella";
+
+    }
+
+    public void move(){
+        System.out.println("I am jumping over puddle");
+    }
+}
+
+
+
 public class Polimorfismo{
 
     public static void main(String[] args){
         Octocat mona = new Octocat();
         mona.greet();
         mona.move();
-
         System.out.println();
 
         DinoCat dino = new DinoCat();
         dino.greet();
         dino.move();
+        System.out.println();
+
+        UmbrellaCat umbrella = new UmbrellaCat();
+        umbrella.greet();
+        umbrella.move();
+        System.out.println();
+        
         
     }
 }
