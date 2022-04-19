5ee953650db2b87709f9d161796b2575468dc971
grayTurtle01 <the.correo@protonmail.com>
2022-04-19 11:02:53 -0500

    add: Moves interface

--- 3_Interfaces/Interfaces.java
+++ 3_Interfaces/Interfaces.java
@@ -19,6 +19,14 @@ class Champion{
      }
 }
 
+interface Moves{
+    void pasive(String name, String description);
+    void firstMove_Q(String name, String description);
+    void secondMove_W(String name, String description);
+    void thirdMove_E(String name, String description);
+    void ultimate_R(String name, String description);
+}
+
 public class Interfaces{
 
     public static void main(String[] args){
