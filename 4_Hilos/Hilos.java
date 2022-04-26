4bd3a9d62ef6b9b9585aeee30555c306e755c2e9
grayTurtle01 <the.correo@protonmail.com>
2022-04-26 16:47:36 -0500

    new images champs added

--- 4_Hilos/Hilos.java
+++ 4_Hilos/Hilos.java
@@ -28,10 +28,10 @@ public class Hilos{
     public static void main(String[] args) throws InterruptedException{
         Ventana v = new Ventana();
         ImageIcon newIcon;
-        String[] fileNames = {"KhaZix.jpg", "akali.jpg", "leeSin.jpg", "Wukong.jpg"};
+        String[] fileNames = {"KhaZix.jpg", "akali.jpg", "leeSin.jpg", "Wukong.jpg", "Sett.jpg", "Annie.jpg"};
 
         for(String fileName : fileNames){
-            Thread.sleep(1000);
+            Thread.sleep(1500);
             newIcon = new ImageIcon("images_large/"+fileName);
             v.image.setIcon(newIcon);
         }
