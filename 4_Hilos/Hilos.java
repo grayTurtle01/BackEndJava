7f548dc0b85f368b82bd0c0202d6e9967223fa8f
grayTurtle01 <the.correo@protonmail.com>
2022-04-26 18:42:30 -0500

    mafalda

--- 4_Hilos/Hilos.java
+++ 4_Hilos/Hilos.java
@@ -28,11 +28,18 @@ public class Hilos{
     public static void main(String[] args) throws InterruptedException{
         Ventana v = new Ventana();
         ImageIcon newIcon;
-        String[] fileNames = {"KhaZix.jpg", "akali.jpg", "leeSin.jpg", "Wukong.jpg", "Sett.jpg", "Annie.jpg"};
+        //~ String[] fileNames = {"KhaZix.jpg", "akali.jpg", "leeSin.jpg", "Wukong.jpg", "Sett.jpg", "Annie.jpg"};
+        //~ String directory = "images_large/";
+        String directory = "mafalda/";
+        String[] fileNames = {"mafalda01.png", "mafalda02.png", "mafalda03.png", "mafalda04.png"};
+        
 
-        for(String fileName : fileNames){
-            Thread.sleep(1500);
-            newIcon = new ImageIcon("images_large/"+fileName);
+        //~ for(String fileName : fileNames){
+        String fileName = "";
+        for(int i = 1; i <= 8; i++){
+            fileName = "mafalda0"+i+".png";
+            Thread.sleep(3000);
+            newIcon = new ImageIcon(directory + fileName);
             v.image.setIcon(newIcon);
         }
 
